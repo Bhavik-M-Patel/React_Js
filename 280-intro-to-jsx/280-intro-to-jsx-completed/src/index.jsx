0498c9f07bcaef85fe728d225d5c258eb00ee4bf
Bhavik M Patel <bhavikpatel1312002@gmail.com>
2024-09-12 16:05:05 +0530

    first commit

---
+++ 280-intro-to-jsx/280-intro-to-jsx-completed/src/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <div>
+  <h1>hello, Bhavik</h1>
+  <p>How're You?</p>
+  </div>
+  ,document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
